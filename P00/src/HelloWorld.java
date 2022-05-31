d74ea4de617cd3c488253867ac72a8fad603b81b
nrlandrini <21021749@myrp.edu.sg>
2022-05-31 18:20:35 +0800

    push to P00

---
+++ P00/src/HelloWorld.java
@@ -0,0 +1,20 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello,World");
+	}
+	
+	public static int findMax(int arr[]) {
+		int max=0;
+		for(int i = 0; i < arr.length;i++) {
+			if(max < arr[i]) {
+				max = arr[i];
+			}
+			return max;
+		}
+		return max;
+	}
+
+}
