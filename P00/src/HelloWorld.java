362ee04303acd192b845b49d6db76004b6f84794
nrlandrini <21021749@myrp.edu.sg>
2022-07-19 14:17:50 +0800

    done

--- P00/src/HelloWorld.java
+++ P00/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello,World");
+		System.out.println("2 + 2 = 4");
 	}
 	
 	public static int findMax(int arr[]) {
@@ -16,5 +17,7 @@ public class HelloWorld {
 		}
 		return max;
 	}
+		
+	
 
 }
